5ac3a1387f706f5880b3f0cddeae4b637c509774
Juanjo Roman Bernal <juanjoroman1010@gmail.com>
2026-09-02 23:16:13 -0500

    Javascript : variables en js

--- 05-IntroduccionJS/js/01-script.js
+++ 05-IntroduccionJS/js/01-script.js
@@ -1,7 +1,53 @@
 // comentario
 
 
-/* Variables */
+/* Variables
 
-var producto = 'Audifonos Gamer';
+------------NO SE PUEDE HACER EN JS----------------
 
+TENER NUMEROS AL INICIO DE UNA VARIABLE
+TENER ESPACIOS
+GUIONES
+
+
+
+
+*/
+
+
+
+
+/* Forma vieja de declarar variables */
+var producto = 'Audifonos Gamer'; /* inicializar variable con valor */
+
+var disponible; /* inicializar variable sin valor */
+
+/*
+producto = true;  reasiganando valor */
+
+disponible = true;
+
+
+
+var producto1 = 'compuadora',
+    disponible1 = true,
+    categoria = 'compuadoras';  /* Declarar 3 variables al mismo tiempo */
+    
+    
+    
+    
+    
+    
+/* eStilos de variables */
+
+
+var nombre_producto = 'monitor HD';/* underscode */
+var nombreProducto = 'monitor HD';/* camelcase */
+var NombreProducto = 'monitor HD'; /* PascalCase */
+var nombreproductp = 'monitor HD';/* lowercase */
+
+
+
+
+
+console.log(producto);
