1aa5f77a20f8ca556c3b33f71989371e80c3651c
Juanjo Roman Bernal <juanjoroman1010@gmail.com>
2026-09-14 10:06:04 -0500

    Javascript : numeros en Js

---
+++ 05-IntroduccionJS/js/05-script.js
@@ -0,0 +1,19 @@
+// numeros
+
+
+/* 
+            * no hay que especificar el tipo de datos para numeros , js todo lo toma como numeros
+
+*/
+
+const nombre = " JUAN";
+const numero1 = 100;
+const numero2 = 200;
+
+
+
+console.log(numero1);
+console.log(numero2);
+console.log(nombre);
+
+console.log(numero1 + numero2);
