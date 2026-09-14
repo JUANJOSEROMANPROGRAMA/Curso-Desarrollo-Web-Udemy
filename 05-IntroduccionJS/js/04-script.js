7014cc9de406ef36d54a25bfd55b07d3206d1033
Juanjo Roman Bernal <juanjoroman1010@gmail.com>
2026-09-14 09:46:45 -0500

    Javascript : Cadenas de texto

---
+++ 05-IntroduccionJS/js/04-script.js
@@ -0,0 +1,8 @@
+const producto = 'Monitor de 20  "pulgadas"'; //String o cadena de texto    ESTA ES LA QUE MAS SE UTILIZA
+const producto1 = String ('Monitor de 30 pulgadas'); //String o cadena de texto
+const producto3 = new String ('Monitor de 40 pulgadas');
+
+
+console.log(producto);  
+console.log(producto1);  
+console.log(producto3);  
