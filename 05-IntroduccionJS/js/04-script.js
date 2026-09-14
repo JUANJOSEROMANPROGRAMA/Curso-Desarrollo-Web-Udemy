015072e08ff08bcb599ae8a3c113d46e222789f2
Juanjo Roman Bernal <juanjoroman1010@gmail.com>
2026-09-14 09:55:27 -0500

    JavaScript: metodos para strings

--- 05-IntroduccionJS/js/04-script.js
+++ 05-IntroduccionJS/js/04-script.js
@@ -1,8 +1,17 @@
-const producto = 'Monitor de 20  "pulgadas"'; //String o cadena de texto    ESTA ES LA QUE MAS SE UTILIZA
+const tweet = 'Aprendiendo JavaScript con el curso de Desarrollo Web Completo"'; //String o cadena de texto    ESTA ES LA QUE MAS SE UTILIZA
 const producto1 = String ('Monitor de 30 pulgadas'); //String o cadena de texto
-const producto3 = new String ('Monitor de 40 pulgadas');
 
 
-console.log(producto);  
+console.log(tweet.length);// .length para enumerar el  tamaño de caracteres en cade de texto
 console.log(producto1);  
-console.log(producto3);  
+
+// IndexOf    para ver en que posicion se encuuentra un texto que se busca
+
+console.log(tweet.indexOf('curso')); // retorna la posicion en la que se encuentra 
+console.log(producto1.indexOf('curso'));  // si retorna -1 es porque no se encuentra el texto que se busca
+
+
+
+//Include   (es tipo boleando retorna true o false)
+console.log(tweet.includes('curso'));
+console.log(producto1.includes('curso'));  // si retorna -1 es porque no se encuentra el texto que se busca
