2725ea991438d576baa1d3dcf795e6ba91107c59
Juanjo Roman Bernal <juanjoroman1010@gmail.com>
2026-09-15 18:21:41 -0500

    Javascript:concatenacion y template strings

---
+++ 05-IntroduccionJS/js/08-script.js
@@ -0,0 +1,19 @@
+// Concatenacion y template strings
+
+
+
+const nombre = "Juan José Román Bernal";
+const email = 'juanjoroman1010@gmail.com';
+
+
+console.log(nombre + " " + email);// concatenacion
+
+
+//template Strings -Strings literal
+
+console.log(`Nombre de cliente : ${nombre}  Email es : ${email}`);
+
+
+
+
+
