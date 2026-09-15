bf794cbe0dac460805f3071e81dd800ab725b217
Juanjo Roman Bernal <juanjoroman1010@gmail.com>
2026-09-15 18:02:15 -0500

    Javascript : Funciones con Math

---
+++ 05-IntroduccionJS/js/06-script.js
@@ -0,0 +1,45 @@
+/* OBJETO MATH 
+
+
+en la consoloa de js , utilizar comando window para mirar todods los comandos
+
+*/
+
+window.Math.random();
+
+
+
+let resultado;
+let resultado1;
+let resultado2;
+let resultado3;
+let resultado4;
+let resultado5;
+
+let resultado6;
+
+
+
+// imprimir el numero ´PI
+resultado = Math.PI;
+resultado1 = Math.round(2.45); // para redondear numeos
+resultado2 = Math.ceil(10.45); // para redondear hacia arriba
+resultado3 = Math.floor(3.45); // para redondear hacia abajo
+resultado4 = Math.sqrt(16); // raiz cuadrada de un numeros
+
+
+resultado = Math.abs(-3);// valor abssoluto de un numero
+resultado = Math.random();
+resultado = Math.max(1,2,3,4,5,6);//sacar el maximo
+resultado = Math.min(1,23,4,5,6);// sacar el minimo
+
+
+
+console.log(resultado);
+console.log(resultado1);
+console.log(resultado2);
+console.log(resultado3);
+console.log(resultado4);
+
+
+
