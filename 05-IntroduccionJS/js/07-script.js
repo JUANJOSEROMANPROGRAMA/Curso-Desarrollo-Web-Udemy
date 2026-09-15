487723220a2c6b5e76158ccd5ef5edf89cd2a6a5
Juanjo Roman Bernal <juanjoroman1010@gmail.com>
2026-09-15 18:11:56 -0500

    Javascript :Incrementos y decrementos

---
+++ 05-IntroduccionJS/js/07-script.js
@@ -0,0 +1,41 @@
+// Orden de las Operaciones
+
+/* 
+El orden de las operaciones en JS es el mismo que en matematicas de la vidia diaria
+
+*/
+
+
+let resultado;
+
+
+resultado = (20 + 30 )* 2 ;
+
+
+console.log(resultado);
+
+
+
+//Incrementos 
+
+let puntaje = 10;
+
+puntaje++;
+
+console.log(puntaje);
+
+//decrementos 
+let puntaje1 =10;
+
+puntaje1--;// para decrementar 1 al valor guardado
+console.log(puntaje1);
+
+
+/* 
+si se desea que se incremente antes de mostrar seria ++puntaje
+si se desea que se imprima primero y luego se incremente seria puntaje++
+
+
+
+
+*/
