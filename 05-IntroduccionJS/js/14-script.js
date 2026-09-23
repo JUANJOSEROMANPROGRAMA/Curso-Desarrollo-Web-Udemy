c6b37536799f1ea613e00f45d040c9839f51490c
Juanjo Roman Bernal <juanjoroman1010@gmail.com>
2026-09-23 11:13:15 -0500

    Javascript : metodos para Arreglos

--- 05-IntroduccionJS/js/14-script.js
+++ 05-IntroduccionJS/js/14-script.js
@@ -1,4 +1,8 @@
-// Arreglos o Arrays
+
+
+
+
+//**************************** ARREGLOS O ARRAYS      ***************************
 
 
 
@@ -26,9 +30,62 @@ console.log(meses[8])  //los arreglos en js empiezan desde 0
 console.log(arreglo.length)
 
 
-//iteraador foreach
 
-meses.forEach(function(meses){
+
+
+
+//****************************     //iteraador foreach  ***************************/* 
+
+/* meses.forEach(function(meses){
 
 console.log(meses);
-})
+})  */
+
+
+// ***************METODOS DE ARREGLO*****************
+
+        //***************AGREGAR*****************
+numeros.push(1010)//agregar un numero al final de arreglo
+
+numeros.push(11,12,23,34)//varios numeros a la  vez
+
+
+numeros.unshift(-1,-2,-3)// agregar numeros al inicio del arreglo
+
+
+
+
+
+
+
+
+
+
+//****************************   ELIMINAR    ***************************
+
+meses.pop() //elimima eñ ultimo elemto del arreglo
+meses.shift()// elimina el primero del arreglo
+
+meses.splice(2,1)//recorre el arreglo, llega la posicion 2 y elimina un elemento a la derecha
+
+
+console.table(meses)
+console.table(numeros)
+
+
+
+
+
+
+
+//****************************    Rest Operator O Spreand Operator   ***************************
+
+
+const NuevoArreglo = [...meses, 'Junio'];  // copia el arreglo de meses y le asigana el nombre NuevoArreglo, luego le agrega el valor al final del nuevo arreglo
+
+const NuevoArreglo1 = ['Junio',...meses]; // este hace lo mismo pero agrega al inicio
+
+
+
+console.log(NuevoArreglo)
+console.log(NuevoArreglo1)
